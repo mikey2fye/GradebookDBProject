e5a065099b056d3365c9c13d65f87200b2275f86
pernellleamon03 <119986791+pernellleamon03@users.noreply.github.com>
2024-04-16 15:42:08 -0400

    Create Task9_AddPointsToAssignmentScores.sql

---
+++ Task9_AddPointsToAssignmentScores.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS AddPointsToAssignmentScores;
+
+DELIMITER //
+
+CREATE PROCEDURE AddPointsToAssignmentScoresInCourse(
+    IN course_id_param INT,
+    IN assignment_name_param VARCHAR(100),
+    IN points_to_add INT
+)
+BEGIN
+    UPDATE Grades
+    SET score = score + points_to_add
+    WHERE assignment_name = assignment_name_param
+    AND course_id = course_id_param;
+END //
+
+DELIMITER ;
+
+CALL AddPointsToAssignmentScoresInCourse(14216, 'Homework 1', 2); -- Example call to add 2 points to each student's score on 'Homework 1' in course 14216
