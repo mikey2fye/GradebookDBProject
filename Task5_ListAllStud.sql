20515485b99e0de3a4411610dc743c113664c2f5
pernellleamon03 <119986791+pernellleamon03@users.noreply.github.com>
2024-04-16 15:06:41 -0400

    Update Task5_ListAllStud.sql

--- Task5_ListAllStud.sql
+++ Task5_ListAllStud.sql
@@ -1,5 +1,16 @@
-SELECT s.student_id, s.first_name, s.last_name, c.course_id, c.course_name
-FROM Students s
-JOIN Grades g ON s.student_id = g.student_id
-JOIN Courses c ON g.course_id = c.course_id
-WHERE c.course_id = 14216; -- Change the course_id to the desired course
+DROP PROCEDURE IF EXISTS GetStudentsInCourse;
+
+DELIMITER //
+
+CREATE PROCEDURE GetStudentsInCourse(IN course_id_param INT)
+BEGIN
+    SELECT DISTINCT s.student_id, s.first_name, s.last_name, c.course_id, c.course_name
+    FROM Students s
+    JOIN Grades g ON s.student_id = g.student_id
+    JOIN Courses c ON g.course_id = c.course_id
+    WHERE c.course_id = course_id_param;
+END //
+
+DELIMITER ;
+
+CALL GetStudentsInCourse(14216); -- Change the course_id to the desired course
