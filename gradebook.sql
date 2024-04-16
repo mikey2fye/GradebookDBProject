66a5ef9559472ea22a7c36527b0fafbd12fc7c21
Michael J. Johnson Jr <mjohnson12279@gmail.com>
2024-04-16 10:56:31 -0400

    Update gradebook.sql

--- gradebook.sql
+++ gradebook.sql
@@ -1,28 +1,31 @@
-CREATE TABLE Courses (
-    course_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Courses (
+    course_id INT,
     department VARCHAR(50),
     course_number VARCHAR(10),
     course_name VARCHAR(100),
     semester VARCHAR(20),
-    year INT
+    year INT,
+    course_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE Assignments (
-    assignment_id SERIAL PRIMARY KEY,
-    course_id INT,
+CREATE TABLE IF NOT EXISTS Assignments (
+    assignment_id INT,
+    course_id INT(5),
     category VARCHAR(50),
     weight DECIMAL(5, 2),
     assignment_name VARCHAR(100),
-    FOREIGN KEY (course_id) REFERENCES Courses(course_id)
+    FOREIGN KEY (course_id) REFERENCES Courses(course_id),
+    assignment_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE Students (
-    student_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Students (
+    student_id INT,
     first_name VARCHAR(50),
-    last_name VARCHAR(50)
+    last_name VARCHAR(50),
+    student_id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE Grades (
+CREATE TABLE IF NOT EXISTS Grades (
     grade_id SERIAL PRIMARY KEY,
     student_id INT,
     assignment_id INT,
@@ -31,12 +34,12 @@ CREATE TABLE Grades (
     FOREIGN KEY (assignment_id) REFERENCES Assignments(assignment_id)
 );
 
-INSERT INTO Courses (department, course_number, course_name, semester, year) VALUES
-('Computer Science', 'CSCI350', 'Structure of Program Language', 'Spring', '2024'),
-('Computer Science', 'CSCI432', 'Database Systems', 'Spring', '2024'),	
-('Computer Science' , 'CSCI454', 'Intro to Cyber Security II', 'Spring', '2024'),	
-('Computer Science', 'CSCI482', 'Natural Language Processing', 'Spring', '2024'),	
-('Computer Science', 'EGPP493',	'Bison Startup', 'Spring', '2024');
+INSERT INTO Courses (department, course_id, course_number, course_name, semester, year) VALUES
+('Computer Science', '14216', 'CSCI350', 'Structure of Program Language', 'Spring', '2024'),
+('Computer Science', '14150', 'CSCI432', 'Database Systems', 'Spring', '2024'),	
+('Computer Science' , '17814', 'CSCI454', 'Intro to Cyber Security II', 'Spring', '2024'),	
+('Computer Science', 18107, 'CSCI482', 'Natural Language Processing', 'Spring', '2024'),	
+('Computer Science', '13778', 'EGPP493', 'Bison Startup', 'Spring', '2024');
 
 INSERT INTO Assignments (course_id, category, weight, assignment_name) VALUES
 (18107, 'Attendance', 10, 'Class Attendance'),
@@ -46,11 +49,12 @@ INSERT INTO Assignments (course_id, category, weight, assignment_name) VALUES
 (2, 'Homework', 30, 'Assignment 1'),
 (2, 'Tests', 70, 'Midterm Exam');
 
-INSERT INTO Students (first_name, last_name) VALUES
-('John', 'Doe'),
-('Pernell', 'Leamon'),
-('Jane', 'Smith'),
-('Michael', 'Johnson');
+INSERT INTO Students (student_id, first_name, last_name) VALUES
+(03034572, 'John', 'Doe'),
+(03032573, 'Bernell', 'Deamon'),
+(03052583, 'Jane', 'Smith'),
+(03062173, 'Jichael', 'Mohnson'),
+(03032573, 'Evan', 'Ibhawoa');
 
 INSERT INTO Grades (student_id, assignment_id, score) VALUES
 (1, 1, 8.5),
